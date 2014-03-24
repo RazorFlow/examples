fddbe23391100032c90ede546f15c666978b41cf
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2014-03-24 17:27:54 +0530

    Modified pie chart example to take onItemClick

--- src/js/examples/chart_pie.js
+++ src/js/examples/chart_pie.js
@@ -4,5 +4,8 @@ rf.StandaloneDashboard(function(db){
 	chart.setCaption("A pie chart");
 	chart.setLabels (["Jan", "Feb", "Mar"]);
 	chart.setPieValues ([10, 14, 13]);
+    chart.onItemClick(function(obj) {
+        console.log(obj);
+    });
 	db.addComponent (chart);
 });
