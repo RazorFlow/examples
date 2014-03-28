fe112fdea7bd1f8b2c4783fcebee4cdfb45cf5c3
Swaroop SM <swaroop@razorflow.com>
2014-03-28 12:00:11 +0530

    ported testcases to use buildDashboard function

--- src/php/testcases/kpi1.php
+++ src/php/testcases/kpi1.php
@@ -1,7 +1,7 @@
 <?php
 
 class SampleDashboard extends StandaloneDashboard {
-    public function initialize () {
+    public function buildDashboard () {
         $kpi = new KPIComponent("kpi1");
         $kpi->setCaption("Hello world");
         $kpi->setValue(42);
--- src/php/testcases/table.php
+++ src/php/testcases/table.php
@@ -1,7 +1,7 @@
 <?php
 
 class SampleDashboard extends StandaloneDashboard {
-    public function initialize () {
+    public function buildDashboard () {
         $table = new TableComponent('table1');
         $table->setCaption("Table 1");
         $table->setDimensions (6,6);
