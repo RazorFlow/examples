99422600daa967f6f3c827738b6b27073da46f85
Swaroop SM <swaroop@razorflow.com>
2014-03-29 16:49:25 +0530

    edited chart_updatevals php example

--- src/php/examples/chart_updatevals.php
+++ src/php/examples/chart_updatevals.php
@@ -6,16 +6,24 @@ class SampleDashboard extends StandaloneDashboard {
     $chart->setCaption("My First Chart");
     $chart->setDimensions (4, 4);
     $chart->setLabels (["Jan", "Feb", "Mar"]);
-    $chart->addSeries ("packaged_foods", "Packaged Foods", [1513, 976, 1321]);
+    $chart->addSeries ("beverages", "Beverages", array(1355, 1916, 1150));
+    $chart->addSeries ("packaged_foods", "Packaged Foods", array(1513, 976, 1321));
 
     $this->addComponent ($chart);
 
-    $chart->onItemClicked (array($chart), "handleItemClick");
+    $chart2 = new ChartComponent("chart2");
+    $chart2->setDimensions (4, 4);
+    $chart2->setCaption ("My First Chart");
+    $chart2->setLabels (array("A", "B", "C"));
+    $chart2->addSeries ("series_1", "Series 1", array(1, 2, 3));
+    $this->addComponent ($chart2);
+
+    $chart->onItemClicked (array($chart2), "handleItemClick");
   }
 
   public function handleItemClick ($source, $targets, $params) {
-    $chart = $this->getComponentByID("chart1");
-    $chart->updateSeries("packaged_foods", [1, 2, 3]);
+    $chart2 = $this->getComponentByID("chart2");
+    $chart2->updateSeries("series_1", [3, 5, 2]);
   }
 
 }
