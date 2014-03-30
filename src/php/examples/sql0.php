fe44e88674fdccb928c4e1adef5c3cf87714d075
Anirudh Sanjeev <anirudh@razorflow.com>
2014-03-30 21:55:39 +0530

    updated sql example and added a new chart update example

--- src/php/examples/sql0.php
+++ src/php/examples/sql0.php
@@ -24,32 +24,33 @@ class SampleDashboard extends StandaloneDashboard {
 
   public function buildDashboard () {
     $this->setDashboardTitle ("SQL Demo Dashboard");
-  	$chart = new ChartComponent ('c1');
-  	$chart->setDimensions (6, 6);
-  	$chart->setCaption ("Top 5 Artists by Revenue");
+  	$topArtistsChart = new ChartComponent ('c1');
+  	$topArtistsChart->setDimensions (6, 6);
+  	$topArtistsChart->setCaption ("Top 5 Artists by Revenue");
   	$top_artists = $this->getTopArtists();
-  	$chart->setLabels (ArrayUtils::pluck($top_artists, "Name"));
-  	$chart->addSeries ('series_1', "Top Artists", ArrayUtils::pluck($top_artists, "total_sales"));
-  	$this->addComponent($chart);
+  	$topArtistsChart->setLabels (ArrayUtils::pluck($top_artists, "Name"));
+  	$topArtistsChart->addSeries ('top_artists', "Top Artists", ArrayUtils::pluck($top_artists, "total_sales"));
+  	$this->addComponent($topArtistsChart);
 
   	$topAlbumsChart = new ChartComponent ('c2');
   	$topAlbumsChart->setDimensions (6, 6);
   	$topAlbumsChart->setCaption ("Top 5 Albums by Revenue");
   	$top_albums = $this->getTopAlbums();
   	$topAlbumsChart->setLabels (ArrayUtils::pluck($top_albums, "Title"));
-  	$topAlbumsChart->addSeries ('series_1', "Top Albums", ArrayUtils::pluck($top_albums, "total_sales"));
+  	$topAlbumsChart->addSeries ('top_albums', "Top Albums", ArrayUtils::pluck($top_albums, "total_sales"));
   	$this->addComponent($topAlbumsChart);
 
-    $chart->bindToEvent ("itemClick", array($topAlbumsChart), "handleArtistChartClick");
+    $topArtistsChart->bindToEvent ("itemClick", array($topAlbumsChart), "handleArtistChartClick");
   }
 
   public function handleArtistChartClick ($source, $target, $params) {
-    $artistName = $params->label;
+    $artistName = $params['label'];
     $topAlbumsChart = $this->getComponentByID("c2");
     $top_albums = $this->getTopAlbums ($artistName);
     $topAlbumsChart->setCaption ("Top 5 albums by ".$artistName);
+    $topAlbumsChart->clearChart();
     $topAlbumsChart->setLabels (ArrayUtils::pluck($top_albums, "Title"));
-    $topAlbumsChart->updateSeries ('series_1', ArrayUtils::pluck($top_albums, "total_sales"));
+    $topAlbumsChart->addSeries('top_albums', "Top Albums", ArrayUtils::pluck($top_albums, "total_sales"));
   }
 }
 
