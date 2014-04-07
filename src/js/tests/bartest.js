fc53daf439b17340a133648094cd596a3e5a8c20
Anirudh Sanjeev <anirudh@razorflow.com>
2014-04-07 18:35:19 +0530

    remove unnecessary files

--- src/js/tests/bartest.js
+++
@@ -1,5 +0,0 @@
-describe ("It should do other stuff", function () {
-	it("Should be ok as well", function () {
-		expect(1).toEqual(1);
-	})
-})
--- src/js/tests/footest.js
+++
@@ -1,5 +0,0 @@
-describe ("It should do stuff", function () {
-	it("Should be ok", function () {
-		expect(1).toEqual(1);
-	})
-})
