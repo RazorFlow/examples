160238038e2717292e2ee95a1cb8d9f391d65e47
Swaroop SM <swaroop.striker@gmail.com>
2014-05-20 16:47:13 +0530

    updated chart_event example

--- src/php/examples/chart_event.php
+++ src/php/examples/chart_event.php
@@ -1,11 +1,19 @@
 <?php
 
 class SampleDashboard extends StandaloneDashboard {
+
+  private $labels;
+
+  public function __construct() {
+    //Initialize variables
+    $this->labels = array("2009", "2010", "2011");
+  }
+
   public function buildDashboard(){
     $chart = new ChartComponent("chart");
     $chart->setCaption("Expenses incurred for Food Consumption by Year");
     $chart->setDimensions (4, 4);
-    $chart->setLabels (["2009", "2010", "2011"]);
+    $chart->setLabels ($this->labels);
     $chart->addSeries ("beverages", "Beverages", [1355, 1916, 1150], array('numberPrefix' => '$'));
     $chart->addSeries ("packaged_foods", "Packaged Foods", [1513, 976, 1321], array('numberPrefix' => '$'));
     $this->addComponent ($chart);
@@ -20,7 +28,7 @@ class SampleDashboard extends StandaloneDashboard {
 
   public function handleClick ($source, $targets, $params) {
   	$kpi = $this->getComponentById ('kpi');
-  	$kpi->setCaption ("Year: ".$params['label']);
+  	$kpi->setCaption ("Year: ".$this->labels[$params['labelIndex']]);
   	$kpi->setValue ($params['value'], array('numberPrefix' => '$'));
   }
 }
