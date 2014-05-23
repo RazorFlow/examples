e0f049918031224e1f6fe9c3e9477be6098d7eb0
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2014-05-23 13:53:57 +0530

    Chart custom colors testcase

--- src/js/testcases/chart_custom_colors.js
+++ src/js/testcases/chart_custom_colors.js
@@ -6,7 +6,7 @@ StandaloneDashboard(function (db) {
     c4.addSeries("seriesA", "Series A", [1, 3, 5, 1, 9], {seriesDisplayType: 'column', numberPrefix: '$'});
     c4.addSeries("seriesB", "Series B", [3, 1, 9, 2, 3], {seriesDisplayType: 'column'});
     c4.addSeries("seriesC", "Series A", [4, 5, 5, 7, 9], {seriesDisplayType: 'column', seriesColor: 'green'});
-    c4.addSeries("seriesD", "Series A", [1, 3, 6, 1, 4], {seriesDisplayType: 'line', seriesColor: 'rgb(0, 255, 255)'});
-    c4.addSeries("seriesE", "Series B", [2, 1, 9, 2, 3], {seriesDisplayType: 'line', seriesColor: '#F0F'});
+    c4.addSeries("seriesD", "Series A", [1, 3, 6, 1, 4], {seriesDisplayType: 'column', seriesColor: 'rgb(0, 255, 255)'});
+    c4.addSeries("seriesE", "Series B", [2, 1, 9, 2, 3], {seriesDisplayType: 'column', seriesColor: '#F0F'});
     db.addComponent(c4);
 });
