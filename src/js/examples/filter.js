c5c963602c6d28853d12a2488fa394e99639727d
Swaroop SM <swaroop.striker@gmail.com>
2014-05-26 13:33:34 +0530

    modified filter example to get individual items

--- src/js/examples/filter.js
+++ src/js/examples/filter.js
@@ -13,4 +13,13 @@ rf.StandaloneDashboard(function (db) {
     });
 
     db.addComponent(c1);
+
+    c1.onApplyClick(function() {
+        console.log('Name: ' + c1.getInputValue('name'));
+        console.log('Products: ' , c1.getInputValue('products'));
+        console.log('Cities: ' , c1.getInputValue('cities'));
+        console.log('Delivery Date: ' + c1.getInputValue('delivery_date'));
+        console.log('Grace Period: ' + c1.getInputValue('grace_period'));
+        console.log('Units: ' , c1.getInputValue('units'));
+    });
 });
