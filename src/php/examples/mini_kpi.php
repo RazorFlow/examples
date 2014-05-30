703822493513a5f6b59281651cb9dacbc3e2bb20
Swaroop SM <swaroop.striker@gmail.com>
2014-05-30 11:59:02 +0530

    update mini kpi testcase

--- src/php/examples/mini_kpi.php
+++ src/php/examples/mini_kpi.php
@@ -4,18 +4,30 @@ class SampleDashboard extends StandaloneDashboard {
   public function buildDashboard(){
     $chart = new ChartComponent("my_first_chart");
     $chart->setCaption("Expenses incurred on Food Consumption by Year");
-    $chart->setDimensions (6, 6);
+    $chart->setDimensions (8, 8);
     $chart->setLabels (["2009", "2010", "2011"]);
     $chart->addSeries ("beverages", "Beverages", [1355, 1916, 1150]);
     $chart->addSeries ("packaged_foods", "Packaged Foods", [1513, 976, 1321]);
 
     $chart->addComponentKPI("first", array(
-      "caption" => "Hello",
-      "value" => 20
+      "caption" => "Number Formatter",
+      "value" => 4200000,
+      "numberPrefix" => "$",
+      "numberHumanize" => true
     ));
     $chart->addComponentKPI("second", array(
-      "caption" => "Bye",
-      "value" => 114
+      "caption" => "Long Text with blah blah blah",
+      "value" => 200,
+      "numberForceDecimals" => true
+    ));
+    $chart->addComponentKPI("third", array(
+      "caption" => "new",
+      "value" => 20,
+      "numberSuffix" => "%"
+    ));
+    $chart->addComponentKPI("forth", array(
+      "caption" => "again",
+      "value" => 100
     ));
 
     $this->addComponent ($chart);
