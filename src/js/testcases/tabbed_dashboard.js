96df44db270bded0337398d1b2f4eec7fa3cc97f
narendrashetty <narendra@razorflow.com>
2014-06-14 17:06:44 +0530

    added php testcase on table pagination for sql

--- src/js/testcases/tabbed_dashboard.js
+++ src/js/testcases/tabbed_dashboard.js
@@ -8,6 +8,32 @@ StandaloneDashboard(function (tdb) {
     kpi1.setCaption('KPI normal test');
     kpi1.setValue(244);
     db1.addComponent(kpi1);
+    var c1 = new ChartComponent();
+    c1.setCaption("Column Chart");
+    c1.setDimensions(4, 4);
+    c1.setLabels(['January', 'February', 'March', 'April', 'May']);
+    c1.addSeries("seriesA", "Series A", [1, 3, 5, 1, 9], {seriesDisplayType: 'area', numberPrefix: '$'});
+ 
+    db1.addComponent(c1);
+
+    var c2 = new ChartComponent();
+    c2.setCaption("Column Chart multi-series");
+    c2.setDimensions(4, 4);
+    c2.setLabels(['January', 'February', 'March', 'April', 'May']);
+    c2.addSeries("seriesA", "Series A", [1, 3, 5, 1, 9], {seriesDisplayType: 'area', numberPrefix: '$'});
+    c2.addSeries("seriesB", "Series B", [9, 1, 5, 3, 1], {seriesDisplayType: 'area', numberPrefix: '$'});
+
+    db1.addComponent(c2);
+
+    var c3 = new ChartComponent();
+    c3.setCaption("Column Chart multi-series");
+    c3.setDimensions(4, 4);
+    c3.stacked();
+    c3.setLabels(['January', 'February', 'March', 'April', 'May']);
+    c3.addSeries("seriesA", "Series A", [1, 3, 5, 1, 9], {seriesDisplayType: 'area', numberPrefix: '$', seriesStacked: true});
+    c3.addSeries("seriesB", "Series B", [9, 1, 5, 3, 1], {seriesDisplayType: 'area', numberPrefix: '$', seriesStacked: true});
+ 
+    db1.addComponent(c3);
 
 	var db2 = new Dashboard('db2');
 	db2.setDashboardTitle('Dashboard 2');
--- src/js/tests/tabbed_dashboard.js
+++ src/js/tests/tabbed_dashboard.js
@@ -10,7 +10,8 @@ describe ("Basic Chart Tests", function () {
 	var chart_timeout = 400;
 
 	afterEach(function() {
-		db.pro.dispose();
+		db1.pro.dispose();
+		db2.pro.dispose();
 		$(".rfTooltip").remove();
 	});
 
@@ -30,23 +31,115 @@ describe ("Basic Chart Tests", function () {
 		return chart;
 	}
 
-	it("Should display labels on X Axis", function (done) {
-		db = new Dashboard ();
-		var chart = createChart();
+	it("Should display tabbed Dashboard", function (done) {
+		tdb = new TabbedDashboard();
+		db1 = new Dashboard();
+		db2 = new Dashboard();
+		var chart1 = createChart(),
+			chart2 = createChart();
+		db1.setDashboardTitle('Dashboard 1');
+		db2.setDashboardTitle('Dashboard 2');
+		db1.addComponent(chart1);
+		db2.addComponent(chart2);
 
-		db.addComponent(chart);
-		db.embedTo("dbTarget");
+		tdb.addDashboardTab(db1);
+		tdb.addDashboardTab(db2);
+
+		tdb.embedTo("dbTarget");
+
 
 		var th = new TestHelper ();
 		th.start(done)
 		  .wait(chart_timeout)
-		  .setContext(chart.pro.renderer.$core.parent())
-		  .drillContext(".rc-axis:eq(0)")
-		  .assertText("text:eq(0)", "Beverages")
-		  .svgMeasure("text:eq(0)", "y", t3.approximate(14, 1))
-		  .assertText("text:eq(1)", "Vegetables")
+		  .setContext(tdb.pro.$tabDiv)
+		  .drillContext(".tabLinks")
+		  .assertText("li:eq(0) .k-link", "Dashboard 1")
+		  .assertText("li:eq(1) .k-link", "Dashboard 2")
 		  .finish();
 	});
+
+	it("Should click on another Tab", function (done) {
+		tdb = new TabbedDashboard();
+		db1 = new Dashboard();
+		db2 = new Dashboard();
+		var chart1 = createChart(),
+			chart2 = createChart();
+		db1.setDashboardTitle('Dashboard 1');
+		db2.setDashboardTitle('Dashboard 2');
+		db1.addComponent(chart1);
+		db2.addComponent(chart2);
+
+		tdb.addDashboardTab(db1);
+		tdb.addDashboardTab(db2);
+
+		tdb.embedTo("dbTarget");
+
+
+		var th = new TestHelper ();
+		th.start(done)
+		  .wait(chart_timeout)
+		  .setContext(tdb.pro.$tabDiv)
+		  .drillContext(".tabLinks")
+		  .click("li:eq(1)")
+		  .wait(chart_timeout)
+		  .assertClass("li:eq(1)", "k-state-active")
+		  .finish();
+	});
+
+	it("Second tab should be active", function (done) {
+		tdb = new TabbedDashboard();
+		db1 = new Dashboard();
+		db2 = new Dashboard();
+		var chart1 = createChart(),
+			chart2 = createChart();
+		db1.setDashboardTitle('Dashboard 1');
+		db2.setDashboardTitle('Dashboard 2');
+		db1.addComponent(chart1);
+		db2.addComponent(chart2);
+
+		tdb.addDashboardTab(db1);
+		tdb.addDashboardTab(db2, {
+			active : true
+		});
+
+		tdb.embedTo("dbTarget");
+
+
+		var th = new TestHelper ();
+		th.start(done)
+		  .wait(chart_timeout)
+		  .setContext(tdb.pro.$tabDiv)
+		  .drillContext(".tabLinks")
+		  .assertClass("li:eq(1)", "k-state-active")
+		  .finish();
+	});
+
+	it("Second set customized tab title", function (done) {
+		tdb = new TabbedDashboard();
+		db1 = new Dashboard();
+		db2 = new Dashboard();
+		var chart1 = createChart(),
+			chart2 = createChart();
+		db1.setDashboardTitle('Dashboard 1');
+		db2.setDashboardTitle('Dashboard 2');
+		db1.addComponent(chart1);
+		db2.addComponent(chart2);
+
+		tdb.addDashboardTab(db1);
+		tdb.addDashboardTab(db2, {
+			title : "Customized Title"
+		});
+
+		tdb.embedTo("dbTarget");
+
+
+		var th = new TestHelper ();
+		th.start(done)
+		  .wait(chart_timeout)
+		  .setContext(tdb.pro.$tabDiv)
+		  .drillContext(".tabLinks")
+		  .assertText("li:eq(1) .k-link", "Customized Title")
+		  .finish();
 	});
 
 
