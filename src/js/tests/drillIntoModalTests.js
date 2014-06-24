f50816a743df7dd23a3e917aa22b8a37735af8f0
narendrashetty <narendra@razorflow.com>
2014-06-24 12:59:22 +0530

    fixed drill into modals testcase

--- src/js/tests/drillIntoModalTests.js
+++ src/js/tests/drillIntoModalTests.js
@@ -53,11 +53,11 @@ describe ("Basic Chart Tests", function () {
 	      .drillContext(".rc-series-1")
 	      .svgTriggerEvent("rect", "click")
 	      .wait(chart_timeout)
-	      .setContext(".k-window:eq(1)")
+	      .setContext(".k-window")
 	      .drillContext(".rc-axis:eq(0)")
 	      .assertText("text:eq(0)", "Beverages")
 	      .assertText("text:eq(1)", "Vegetables")
-	      .setContext(".k-window:eq(1)")
+	      .setContext(".k-window")
 	      .click(".k-i-close")
 	      .finish();
 	});
