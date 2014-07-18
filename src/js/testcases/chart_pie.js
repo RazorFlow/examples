3673eeae5a5a680c63b46fa6f5bccbbd2acf505b
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2014-07-18 13:52:02 +0530

    pie chart example with number formatting

--- src/js/testcases/chart_pie.js
+++ src/js/testcases/chart_pie.js
@@ -3,6 +3,6 @@ StandaloneDashboard(function (db) {
     pieChart.setDimensions(4,4);
     pieChart.setCaption('Pie Chart');
     pieChart.setLabels (["2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"]);
-    pieChart.addSeries ("beverages", "Beverages", [1355, 1916, 1150, 1355, 1916, 1150, 1355, 1916, 1150, 1355, 1916, 1150], {seriesDisplayType: 'pie'});
+    pieChart.addSeries ("beverages", "Beverages", [1355, 1916, 1150, 1355, 1916, 1150, 1355, 1916, 1150, 1355, 1916, 1150], {seriesDisplayType: 'pie', numberSuffix: ' KB', numberHumanize: true});
     db.addComponent(pieChart);   
 });
